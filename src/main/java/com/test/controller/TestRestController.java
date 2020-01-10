3af097e844e4018ab1338f67265776764143241b
Adrninistrator <Adrninistrator>
2020-01-10 16:53:07 +0800

    删除多余引用

--- src/main/java/com/test/controller/TestRestController.java
+++ src/main/java/com/test/controller/TestRestController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.util.concurrent.atomic.AtomicBoolean;
-
 @RestController
 @RequestMapping("testrest")
 public class TestRestController {
